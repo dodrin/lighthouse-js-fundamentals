71d1d2a5f511741f93ec10ac15152708bad74017
Rina Inada <indrn@icloud.com>
2023-04-19 04:10:35 +0000

    Initial commit

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget yourumbrella! ");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("now you're ready to go outside!");
